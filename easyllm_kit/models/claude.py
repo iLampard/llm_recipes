56b1aaaa9352f0f8b49101c0e7105fc966faa558
iLampard <siqiao_xue@163.com>
2024-10-11 12:00:15 +0800

    add dev container files

--- easyllm_kit/models/claude.py
+++ easyllm_kit/models/claude.py
@@ -1,20 +1,22 @@
-import anthropic
-from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 from easyllm_kit.models.base import LLM
 
 
 @LLM.register('claude_3_sonnet')
 class Claude3Opus(LLM):
     model_name = 'claude_3_sonnet'
+
     def __init__(self, config):
-        self.config = config
-        self.client = anthropic.Anthropic(api_key=self.config.api_key)
+        import anthropic
+        from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+        self.model_config = config['model_config']
+        self.generation_config = config['generation_config']
+        self.client = anthropic.Anthropic(api_key=self.model_config.api_key)
 
     def generate(self, prompt: str, **kwargs):
         response = self.client.messages.create(
-            model=self.model_name,
-            max_tokens=self.config.max_tokens,
-            temperature=self.config.temperature,
+            model=self.model_config.model_name,
+            max_tokens=self.generation_config.max_tokens,
+            temperature=self.generation_config.temperature,
             messages=[
                 {"role": "user", "content": prompt}
             ]
