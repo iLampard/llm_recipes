df5be45e846013af7db21b50f62db41b29fd415d
siqiaoxue <pierre@srp.one>
2024-12-01 18:10:18 +0800

    remove useless code

--- examples/apps/make_app.py
+++ examples/apps/make_app.py
@@ -1,15 +1,3 @@
-import sys
-import os
-
-# Get the directory of the current script
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Navigate to the parent directory (where 'utils' is located)
-parent_dir = os.path.abspath(os.path.join(script_dir, '../..'))
-
-# Add the parent directory to sys.path
-sys.path.append(parent_dir)
-
 from easyllm_kit.utils.app import run_app, create_app
 
 if __name__ == "__main__":
